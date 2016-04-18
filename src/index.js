d78d931f7af10fb94f803b682af20da947b93e0c
scyclow <spike716@gmail.com>
2016-04-18 16:05:45 -0400

    include some assets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.getElementById('app');
-const crap = true;
-export { crap };
+window.onload = function start() {
+  const game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {
+    preload() {
+      game.load.image('hell', 'assets/hell.png');
+      game.load.image('ayeesha', 'assets/ayeesha-sprite.png');
+    },
+
+    create() {
+      let hell = game.add.sprite(game.world.centerX, game.world.centerY, 'hell');
+      hell.anchor.setTo(0.5, 0.5);
+      let ayeesha = game.add.sprite(0, 0, 'ayeesha');
+    }
+  });
+};
